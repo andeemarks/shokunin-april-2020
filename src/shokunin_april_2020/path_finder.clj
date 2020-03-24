49137afc73613b26f7336a439ed0989d981141f5
Andy Marks <vampwillow@gmail.com>
2020-03-24 15:11:11 +1100

    + pseudocode for path finding algo

--- src/shokunin_april_2020/path_finder.clj
+++ src/shokunin_april_2020/path_finder.clj
@@ -2,3 +2,14 @@
 
 (defn path-exists? [office]
     true)
+
+; Flood-fill (node, target-color, replacement-color):
+;  1. If target-color is equal to replacement-color, return.
+;  2. ElseIf the color of node is not equal to target-color, return.
+;  3. Else Set the color of node to replacement-color.
+;  4. Perform Flood-fill (one step to the south of node, target-color, replacement-color).
+;     Perform Flood-fill (one step to the north of node, target-color, replacement-color).
+;     Perform Flood-fill (one step to the west of node, target-color, replacement-color).
+;     Perform Flood-fill (one step to the east of node, target-color, replacement-color).
+;  5. Return.
+(defn flood-fill [x y status])
