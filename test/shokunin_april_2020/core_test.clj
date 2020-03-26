8311159002081c259b8c10630fc4ba7ae05982f4
Andy Marks <vampwillow@gmail.com>
2020-03-27 10:57:44 +1100

    Removing :refer all laziness (Part 3)

--- test/shokunin_april_2020/core_test.clj
+++ test/shokunin_april_2020/core_test.clj
@@ -4,14 +4,14 @@
             [clojure.test.check :as tc]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :as test]
-            [shokunin-april-2020.office :refer :all]
+            [shokunin-april-2020.office :as office]
             [shokunin-april-2020.core :refer :all]))
 
 (test/defspec twer-always-in-back-row
   100
   (prop/for-all [number-of-rows gen/s-pos-int
                  number-of-desks gen/s-pos-int]
-                (= 0 (:row (find-twer (populate-office number-of-rows number-of-desks 1))))))
+                (= 0 (:row (office/find-twer (populate-office number-of-rows number-of-desks 1))))))
 
 (deftest office-population
   (testing "office has specified dimensions"
@@ -24,14 +24,14 @@
       (is (not (= populated-office-1 populated-office-2)))))
 
   (testing "randomly assigns the TWer in a desk in the back row"
-    (is (not (= (:column (find-twer (populate-office 10 10 0.5)))
-                (:column (find-twer (populate-office 10 10 0.5)))
-                (:column (find-twer (populate-office 10 10 0.5)))))))
+    (is (not (= (:column (office/find-twer (populate-office 10 10 0.5)))
+                (:column (office/find-twer (populate-office 10 10 0.5)))
+                (:column (office/find-twer (populate-office 10 10 0.5)))))))
 
   (defn- approx-equals [actual expected tolerance]
     (<= (dec expected) actual (inc expected)))
 
   (testing "populates the office according to the p value"
-    (is (approx-equals (count-population (populate-office 2 5 0.4)) 4 1))
-    (is (approx-equals (count-population (populate-office 10 10 0.2)) 20 1))
-    (is (approx-equals (count-population (populate-office 25 40 0.8)) 800 1))))
+    (is (approx-equals (office/count-population (populate-office 2 5 0.4)) 4 1))
+    (is (approx-equals (office/count-population (populate-office 10 10 0.2)) 20 1))
+    (is (approx-equals (office/count-population (populate-office 25 40 0.8)) 800 1))))
