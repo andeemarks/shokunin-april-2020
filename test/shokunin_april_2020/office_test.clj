eb68b7797eb25c7e25472f80da76bc47a1b4b571
Andy Marks <vampwillow@gmail.com>
2020-04-01 14:17:23 +1100

    Moved all core tests to office

--- test/shokunin_april_2020/office_test.clj
+++ test/shokunin_april_2020/office_test.clj
@@ -1,5 +1,9 @@
 (ns shokunin-april-2020.office-test
   (:require [clojure.test :refer :all]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check :as tc]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as test]
             [shokunin-april-2020.desk :as desk]
             [shokunin-april-2020.location :refer (->Location)]
             [shokunin-april-2020.office :refer :all]))
@@ -40,3 +44,34 @@
     (dotimes [i 100]
       (let [updated-office (place-twer! office)]
         (is (= 0 (:row (find-twer updated-office))))))))
+
+(test/defspec twer-always-in-back-row
+  100
+  (prop/for-all [number-of-rows (gen/fmap inc gen/nat)
+                 number-of-desks (gen/fmap inc gen/nat)]
+                (let [office (populate-office number-of-rows number-of-desks 1)]
+                  (= 0 (:row (find-twer office))))))
+
+(defn- approx [actual expected tolerance]
+  (<= (dec expected) actual (inc expected)))
+
+(deftest office-population
+  (testing "office has specified dimensions"
+    (is (= (count (populate-office 10 15 0.1)) 10))
+    (is (= (count (first (populate-office 10 15 1.0))) 15)))
+
+  (testing "randomly populates the office each time"
+    (let [populated-office-1 (populate-office 3 3 0.4)
+          populated-office-2 (populate-office 3 3 0.4)]
+      (is (not (= populated-office-1 populated-office-2)))))
+
+  (testing "randomly assigns the TWer in a desk in the back row"
+    (is (not (= (:column (find-twer (populate-office 10 10 0.5)))
+                (:column (find-twer (populate-office 10 10 0.5)))
+                (:column (find-twer (populate-office 10 10 0.5)))))))
+
+  (testing "populates the office according to the p value"
+    (is (approx (count-occupied (populate-office 2 5 0.4)) 4 1))
+    (is (approx (count-occupied (populate-office 10 10 0.2)) 20 1))
+    (is (approx (count-occupied (populate-office 25 40 0.8)) 800 1))))
+
--- test/shokunin_april_2020/path_finder_test.clj
+++ test/shokunin_april_2020/path_finder_test.clj
@@ -1,6 +1,5 @@
 (ns shokunin-april-2020.path-finder-test
   (:require [clojure.test :refer :all]
-            [shokunin-april-2020.core :as core]
             [shokunin-april-2020.desk :as desk]
             [shokunin-april-2020.location :refer (->Location)]
             [shokunin-april-2020.office :as office]
